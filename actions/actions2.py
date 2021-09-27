046118be4d171b4d8e75fb15feae7165968e6204
kritika aggarwal <43928250+kritsid@users.noreply.github.com>
2021-09-27 09:41:03 +0530

    basic bot

---
+++ actions/actions2.py
@@ -0,0 +1,31 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionServices(Action):
+
+    def name(self) -> Text:
+        return "action_select_service"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        ser = tracker.slots.get('service_name')
+        if ser.lower() == 'transfer funds':
+            dispatcher.utter_message(response = "utter_transfer_funds")
+        else:
+            dispatcher.utter_message(response = "utter_check_balance")
+
+        return []
