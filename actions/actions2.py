92b6cdc2cef51957d442e89f2e32d44a8ab38103
Ishika Sofat <IshikaSofat@gmail.com>
2021-09-27 09:42:57 +0530

    add actions2.py

--- actions/actions2.py
+++ actions/actions2.py
@@ -7,8 +7,15 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
+from _typeshed import OpenTextModeUpdating
 
+
+# 1. Voice Auth -- local saving
+# 2. Custom Actions -- Kaafi time -- maybe less
+# 3. OTP
+# 4. Alexa
+
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 #
