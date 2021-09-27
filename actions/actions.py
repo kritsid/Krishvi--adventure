7875a3c12c6a320e024f5e08563eadb107c42cae
arjun15091 <aarjun_be18@thapar.edu>
2021-09-27 15:41:07 +0530

    Authentication Part

--- actions/actions.py
+++ actions/actions.py
@@ -2,7 +2,24 @@ from flask import Flask,redirect,url_for,render_template,request,g,session
 # import pandas as pd
 from database import get_db
 from pathlib import Path
+import global_data_config
 import os
+from flask import Flask,render_template,request
+from flask_mail import Mail,Message
+from random import randint
+
+app=Flask(__name__)
+mail=Mail(app)
+
+app.config["MAIL_SERVER"]='smtp.gmail.com'
+app.config["MAIL_PORT"]=465
+app.config["MAIL_USERNAME"]='vipul27goel@gmail.com'
+app.config['MAIL_PASSWORD']='!Hanumanji12'                    #you have to give your password of gmail account
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
+mail=Mail(app)
+otp=randint(000000,999999)
+
 
 
 
@@ -13,10 +30,12 @@ import speech_recognition as s_r
 import scipy.io.wavfile as wav
 import wave
 import os
-from flask_mail import Mail,Message
+# from flask_mail import Mail,Message
 from random import randint
 
 app=Flask(__name__)
+
+app.config['SECRET_KEY'] = os.urandom(24)#for session
 
 
 @app.teardown_appcontext
@@ -55,27 +74,58 @@ def login():
         # groupId='grp_10a96522eb114182bafeb5c51bc40ab6'
         groupId='grp_10a96522eb114182bafeb5c51bc40ab6'
         downloads_path = str(Path.home() / "Downloads")
-        identified_as=my_voiceit.voice_identification(groupId, "no-STT", "never forget tomorrow is a new day", downloads_path+"\welcome.wav")
+        # identified_as=my_voiceit.voice_identification(groupId, "no-STT", "never forget tomorrow is a new day", downloads_path+"\welcome.wav")
+        identified_as=my_voiceit.voice_identification(groupId, "no-STT", "never forget tomorrow is a new day", "pa7.mp4")
         print(identified_as)
         print(type(identified_as))
-        if bool(identified_as):
+        if identified_as['responseCode'] != 'FAIL':
             x=identified_as['userId']
             cursor = get_db()
             cursor.execute('select * from voice_banking_users_db where voice_id = %s',(x,))
             taken = cursor.fetchone()
             if len(taken)>=1:
-                email = taken['email']
-                user_id = taken['id']
-                name = taken['first_name'] +' '+ taken['last_name']
-                account_number = taken['account_number']
-
-                session['user']= taken['email']
-                return render_template('chatroom.html')
+                global_data_config.email = taken['email']
+                global_data_config.user_id = taken['id']
+                global_data_config.name = taken['first_name'] +' '+ taken['last_name']
+                global_data_config.account_number = taken['account_number']
+                global_data_config.balance = taken['balance']
+                global_data_config.account_type = taken['account_type']
+                global_data_config.account_hold = taken['on_hold']
+                print(global_data_config.email)       
+                print(global_data_config.name)   
+                print("*************************")
+                email=global_data_config.email
+                msg=Message(subject='OTP',sender='vipul27goel@gmail.com',recipients=[email])
+                msg.body=str(otp)
+                mail.send(msg)
+                return render_template('verify.html')
+                
             return render_template('voice_home.html')
         else:
-            print("AUTHENTICATION FAILEd")
-            return "AUTHENTICATION FAILEd"
+            print("AUTHENTICATION FAILED")
+            return "AUTHENTICATION FAILED"
     return render_template('voice_home.html')
+
+
+
+
+
+@app.route('/validate',methods=['POST'])
+def validate():
+    user_otp=request.form['otp']
+    if otp==int(user_otp):
+        session['user']= global_data_config.email
+        return render_template('chatroom.html')
+    email=global_data_config.email
+    global_data_config.count+=1
+    msg=Message(subject='OTP',sender='vipul27goel@gmail.com',recipients=[email])
+    msg.body=str(otp)
+    mail.send(msg)
+    if global_data_config.count==2 :
+        return "AUTHENTICATION FAILED"
+    elif global_data_config.count<2 :
+        return render_template('verify.html')
+
 
 if __name__ == '__main__':
     app.run(port = 5055, debug=True)
---
+++ actions/global_data_config.py
@@ -0,0 +1,8 @@
+email =''
+user_id=0
+name = ''
+account_number=11111111
+balance=0
+account_type = ''
+account_hold = ''
+count=0
